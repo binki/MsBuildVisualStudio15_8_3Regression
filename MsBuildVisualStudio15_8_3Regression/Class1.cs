e018a4974ee41098857bb379ed5ee7be9ec2aa96
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2018-09-06 18:08:31 -0400

    Add project files.

---
+++ MsBuildVisualStudio15_8_3Regression/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsBuildVisualStudio15_8_3Regression
+{
+    public class Class1
+    {
+    }
+}
